6eb507e1b7be6d2c7e5450407bf374a04b2d9841
dsrrivera <154566156+dsrrivera@users.noreply.github.com>
2026-07-30 18:07:52 -0600

    Update BlackScholes d1, d2, and computeOptionPrice methods
    d1, d2, computeCallOptionPrice and computePutOptionPrice are updated to allow use beyond initializing class attributes. These methods are used to compute updated d1, d2, and new theoretical price as volatility changes in NewtonRaphson.

--- models/black_scholes.py
+++ models/black_scholes.py
@@ -7,36 +7,50 @@ class BlackScholes:
         self.strike_price = strike_price
         self.time_til_expiration = time_til_expiration  #in years
         self.interest_rate = interest_rate
+        self.option_type = option_type
         self.volatility = volatility
         self.d1 = self.computeD1()
         self.d2 = self.computeD2()
 
-        if option_type == 'call':
+        if self.option_type == 'call':
             self.option_price = self.computeCallOptionPrice()
-        elif option_type == 'put':
+        elif self.option_type == 'put':
             self.option_price = self.computePutOptionPrice()
         else:
             self.option_price = 0.0
-    
-    # 
-    def computeD1(self):
-        numerator = math.log(self.strike_price/self.stock_price) + (self.interest_rate + (self.volatility**2)/2) * self.time_til_expiration
-        denominator = self.volatility*(math.sqrt(self.time_til_expiration))
+
+    # d1 and d2 can be computed using object instance variables or with user set volatility as needed in newton_raphson
+    def computeD1(self, volatility=None):
+        volatility = self.volatility if volatility is None else volatility
+
+        numerator = math.log(self.strike_price/self.stock_price) + (self.interest_rate + (volatility**2)/2) * self.time_til_expiration
+        denominator = volatility*(math.sqrt(self.time_til_expiration))
         d1 = numerator/denominator
         return d1
 
-    def computeD2(self):
-        d2 = self.d1 - (self.volatility * math.sqrt(self.time_til_expiration))
+    # d1 may change in newton_raphson so d1 may be set manually to correctly compute d2
+    def computeD2(self, volatility=None, d1=None):
+        volatility = self.volatility if volatility is None else volatility
+        d1 = self.d1 if d1 is None else d1
+
+        d2 = d1 - (volatility * math.sqrt(self.time_til_expiration))
         return d2
 
-    def computeCallOptionPrice(self):
-        d1_norm = norm.cdf(self.d1)
-        d2_norm = norm.cdf(self.d2)
+    # call/put option prices can be computed using object instance variables or with updated d1 and d2 values needed in newton_raphson
+    def computeCallOptionPrice(self, d1=None, d2=None):
+        d1 = self.d1 if d1 is None else d1
+        d2 = self.d2 if d2 is None else d2
+
+        d1_norm = norm.cdf(d1)
+        d2_norm = norm.cdf(d2)
         theoretical_price = (self.stock_price * d1_norm) - ((math.e**(-self.interest_rate * self.time_til_expiration)) * self.strike_price * d2_norm)
         return theoretical_price
 
-    def computePutOptionPrice(self):
-        d1_norm = norm.cdf(-self.d1)
-        d2_norm = norm.cdf(-self.d2)
+    def computePutOptionPrice(self, d1=None, d2=None):
+        d1 = self.d1 if d1 is None else d1
+        d2 = self.d2 if d2 is None else d2
+
+        d1_norm = norm.cdf(-d1)
+        d2_norm = norm.cdf(-d2)
         theoretical_price = ((math.e**(-self.interest_rate * self.time_til_expiration)) * self.strike_price * d2_norm) - (self.stock_price * d1_norm)
         return theoretical_price
