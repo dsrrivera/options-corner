d6013d34fb3b0e2fe9e12b83519f059b4e746950
dsrrivera <154566156+dsrrivera@users.noreply.github.com>
2026-08-16 02:18:15 -0600

    Add remaining plots to GreeksPlots.jsx and styling
    All the Greeks are now visualized and displayed in a 2x3 grid. Empty charts are displayed as placeholders until data is submitted.

--- frontend/src/components/GreeksPlots.jsx
+++ frontend/src/components/GreeksPlots.jsx
@@ -4,7 +4,11 @@ import Plot from 'react-plotly.js';
 import './GreeksPlots.css'
 
 export default function GreeksPlot({ submittedForm }) {
-  const [plotsData, setPlotsData] = useState(null)
+  const [deltaPlotData, setDeltaPlotData] = useState(null)
+  const [gammaPlotData, setGammaPlotData] = useState(null)
+  const [thetaPlotData, setThetaPlotData] = useState(null)
+  const [vegaPlotData, setVegaPlotData] = useState(null)
+  const [rhoPlotData, setRhoPlotData] = useState(null)
 
   useEffect(() => {
     // nothing is fetched if the form is not submitted, fix later to be empty plots waiting for data
@@ -20,8 +24,12 @@ export default function GreeksPlot({ submittedForm }) {
 
         const result = await plotsResponse.json();
 
-        setPlotsData(JSON.parse(result.delta_plot)); // delta_plot's value is a JSON string so we need to parse it, check main.py /greeks-plot endpoint configuration
-        console.log("PlotsDelta:", plotsData);  
+        // result.plot value is a JSON string so we need to parse it, check main.py /greeks-plot endpoint configuration
+        setDeltaPlotData(JSON.parse(result.delta_plot)); 
+        setGammaPlotData(JSON.parse(result.gamma_plot)); 
+        setThetaPlotData(JSON.parse(result.theta_plot)); 
+        setVegaPlotData(JSON.parse(result.vega_plot)); 
+        setRhoPlotData(JSON.parse(result.rho_plot)); 
 
       } catch (err) {
         console.err('Error fetching Greeks plots:', err);
@@ -31,8 +39,28 @@ export default function GreeksPlot({ submittedForm }) {
   }, [submittedForm]); // reruns every time submittedForm changes
 
   return (
-    // this will change, just a placeholder return for now...
-    <div> {!submittedForm ? (<p>Waiting for options data...</p>) : !plotsData ? (<p>Loading plot...</p>) : (<Plot data={plotsData.data} layout={plotsData.layout}/>)}
+    <div> 
+      {!submittedForm ? 
+        <div className='greeks-plots'>
+        <div className='plot'> <Plot data={[]} layout={{title: 'Empty Chart', xaxis: { title: 'X Axis' }, yaxis: { title: 'Y Axis' }}} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={[]} layout={{title: 'Empty Chart', xaxis: { title: 'X Axis' }, yaxis: { title: 'Y Axis' }}} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={[]} layout={{title: 'Empty Chart', xaxis: { title: 'X Axis' }, yaxis: { title: 'Y Axis' }}} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={[]} layout={{title: 'Empty Chart', xaxis: { title: 'X Axis' }, yaxis: { title: 'Y Axis' }}} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={[]} layout={{title: 'Empty Chart', xaxis: { title: 'X Axis' }, yaxis: { title: 'Y Axis' }}} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={[]} layout={{title: 'Empty Chart', xaxis: { title: 'X Axis' }, yaxis: { title: 'Y Axis' }}} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+      </div> : 
+
+      !deltaPlotData ? (<p>Loading plot...</p>) : 
+      
+      <div className='greeks-plots'>
+        <div className='plot'> <Plot data={deltaPlotData.data} layout={deltaPlotData.layout} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={gammaPlotData.data} layout={gammaPlotData.layout} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={thetaPlotData.data} layout={thetaPlotData.layout} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={vegaPlotData.data} layout={vegaPlotData.layout} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={rhoPlotData.data} layout={rhoPlotData.layout} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+        <div className='plot'> <Plot data={rhoPlotData.data} layout={rhoPlotData.layout} useResizeHandler={true} style={{ width: '100%', height: '100%' }}/> </div>
+      </div>
+      }
     </div>
   );
 }
